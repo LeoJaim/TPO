588880398b978b28edd47eb8a4d6d9642969077a
LeoJaim <leonardo.jaimovich@gmail.com>
2021-09-29 23:42:19 -0300

    Commit 11

--- js/as.js
+++ js/as.js
@@ -1,1 +1,8 @@
 //alert("La resolución de pantalla que tienes en este momento es de: " + window.innerWidth + " x " + window.innerHeight);
+
+//alert("El nombre de la página es: " + pagina);
+
+/*if (this.n == 'index.html') 
+{
+    alert("La resolución de pantalla que tienes en este momento es de: " + window.innerWidth + " x " + window.innerHeight);
+}*/

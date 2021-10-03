605dadc7888b7cb13905f5a0ff5e5adfede2eb88
LeoJaim <leonardo.jaimovich@gmail.com>
2021-10-03 16:39:29 -0300

    Commit13

--- js/as.js
+++ js/as.js
@@ -1,19 +1,10 @@
-//alert("La resolución de pantalla que tienes en este momento es de: " + window.innerWidth + " x " + window.innerHeight);
-
-//alert("El nombre de la página es: " + pagina);
-
-/*if (this.n == 'index.html') 
-{
-    alert("La resolución de pantalla que tienes en este momento es de: " + window.innerWidth + " x " + window.innerHeight);
-}*/
 function loadPage()
  {
      fillCombo(); 
      console.log("ENTRO AL LOAD");
  }
- /**
-* This method fills the dropdownlist with regions information
-*/
+ 
+/* Completo el comobo con los valores del array*/
 function fillCombo(){
  var regions = ["CABA", "Buenos Aires", "Santa Fe","San Juan","Corrientes","Chaco","Misiones","Entre Ríos","Salta","Formosa","La Rioja","Tucumán","Jujuy","Catamarca", "Neuquen","Chubut","Río Negro","La Pampa","Tierra del Fuego", "Stgo. del Estero","Córdoba","San Luis","Mendoza","Santa Cruz"];
  var select = document.getElementById("combo");
@@ -25,9 +16,9 @@ function fillCombo(){
 (function() {
     'use strict';
     window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      // Elijo los formularios que tienen una clase determinada, para ser valdados
       var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
+      // Valido que los campos tengan datos
       var validation = Array.prototype.filter.call(forms, function(form) {
         form.addEventListener('submit', function(event) {
           if (form.checkValidity() === false) {
